9bf61a6fa9b415d52ce397a9531a926c345a2eeb
priambodo.pangestu1 <priambodo.pangestu@primecash.co.id>
2021-10-20 23:58:06 +0700

    Remove unuse function

--- src/services/actions/LoginActions.js
+++ src/services/actions/LoginActions.js
@@ -1,9 +1,7 @@
 import { URL_LOGIN } from "../../helpers/path";
 import base64 from "base-64";
-import { LOGIN_ERROR_SERVER, LOGIN_ERROR_SOMETHING, LOGIN_INVALID_INPUT, POST } from "../../helpers/constant";
+import { LOGIN_ERROR_SERVER, LOGIN_ERROR_SOMETHING, POST } from "../../helpers/constant";
 import { processGetItems } from "./DashboardActions";
-
-let isAuthorized = false;
 
 export function processLogin(uname, pass) {
   let headers = new Headers();
@@ -30,23 +28,4 @@ export function processLogin(uname, pass) {
     .catch(() =>
       alert(LOGIN_ERROR_SOMETHING)
     );
-}
-
-function processLogin2(uname, pass){
-  isAuthorized = handleUserInput(uname, pass);
-  if(isAuthorized){
-    processLogin2(uname, pass);
-  }else{
-    alert(LOGIN_INVALID_INPUT);
-  }
-}
-
-function handleUserInput(uname, pass) {
-  if (!uname || uname !== "test") {
-    return false;
-  } else if (!pass || pass !== "test") {
-    return false;
-  } else {
-    return true;
-  }
 }
